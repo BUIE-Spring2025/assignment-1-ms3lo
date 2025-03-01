49e4f8d40d7b96303ffffc9318a0b0b4170f2260
ms3lo <selimsula@gmail.com>
2025-03-01 15:23:11 +0300

    experimenting the structure

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
-   
+    letter_list = [I,V,X,L,C,D,M]
+    decimal_list = [1000,500,100,50,10,5,1]
+    total_decimal = 0
+    for i in range(num):
+        total_decimal += 1
+
