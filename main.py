b1eb22cf94cf8299f257217414c40762da6cc5f1
ms3lo <selimsula@gmail.com>
2025-03-01 15:41:16 +0300

    fixed letter and decimal lists

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
-    letter_list = [I,V,X,L,C,D,M]
-    decimal_list = [1000,500,100,50,10,5,1]
-    total_decimal = 0
-    for i in range(num):
-        total_decimal += 1
+    letter_list = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+    decimal_list = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    # total_decimal = 0
+
+
+
+
+    # for i in range(num):
+    #   total_decimal += 1
+
+    # if total_decimal == 4:
+    #    th = num[0]
+    #    hnd = num[1]
+    #    ten = num[2]
+    #     one = num[3]
+
+
+    
+
 
